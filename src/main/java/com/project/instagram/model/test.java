9ecef75974f2eea8af50a6da8dbde83890d996d3
Nicky <zenanicole1412@gmail.com>
2019-04-26 23:00:48 -0500

    testAttributes
    testAttributes

--- src/main/java/com/project/instagram/model/test.java
+++ src/main/java/com/project/instagram/model/test.java
@@ -1,4 +1,23 @@
 package main.java.com.project.instagram.model;
 
 public class test {
+
+    private String testNicole;
+    private int testNumber;
+
+    public String getTestNicole() {
+        return testNicole;
+    }
+
+    public void setTestNicole(String testNicole) {
+        this.testNicole = testNicole;
+    }
+
+    public int getTestNumber() {
+        return testNumber;
+    }
+
+    public void setTestNumber(int testNumber) {
+        this.testNumber = testNumber;
+    }
 }
