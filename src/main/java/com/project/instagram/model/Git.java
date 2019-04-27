4b6b98dbd7d72e4d556e0e83296f2a1f1088ee80
Nicky <zenanicole1412@gmail.com>
2019-04-26 23:34:35 -0500

    gitClass
    gitclass

---
+++ src/main/java/com/project/instagram/model/Git.java
@@ -0,0 +1,13 @@
+package main.java.com.project.instagram.model;
+
+public class Git {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
